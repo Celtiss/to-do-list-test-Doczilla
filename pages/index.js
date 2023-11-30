5f5f893fb2f14a38396edb18d32d0503ff8f3385
Elena Temirbekova <temn.lena@mail.ru>
2023-11-30 23:32:54 +0300

    feat: get tasks from api

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,19 @@
+'use strict';
+import {Api} from '../utils/Api.js';
+
 $( "#datepicker" ).datepicker();
+
+// Экземпляр класса Api
+const api = new Api({
+    url: 'http://localhost:3000',
+    headers: {
+        'content-type': 'application/json'
+    }
+});
+
+// Получить задачи
+api.getTasks()
+.then((tasks) => {
+    console.log(tasks);
+})
+.catch(err => console.log(err));
